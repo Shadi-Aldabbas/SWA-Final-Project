dd974d1f2376ff5bde22f5f5e330c4bfcfe1c9e3
Shadi_Aldabbas <dabbasfirst@outlook.com>
2022-06-10 14:36:08 -0500

    created repos

---
+++ vote-service/src/test/java/com/SWAFinalProject/voteservice/VoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SWAFinalProject.voteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
