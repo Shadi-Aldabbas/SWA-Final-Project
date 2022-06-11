672bd95fe73188f73ce34a1ad16c68a12866ce6d
Shadi_Aldabbas <dabbasfirst@outlook.com>
2022-06-10 23:18:21 -0500

    auth service half done

---
+++ Auth-Service/src/main/java/com/SWAFinalProject/AuthService/service/UaaService.java
@@ -0,0 +1,11 @@
+package com.SWAFinalProject.AuthService.service;
+
+
+import com.SWAFinalProject.AuthService.model.LoginRequest;
+import com.SWAFinalProject.AuthService.model.LoginResponse;
+import com.SWAFinalProject.AuthService.model.RefreshTokenRequest;
+
+public interface UaaService {
+    LoginResponse login(LoginRequest loginRequest);
+    LoginResponse refreshToken(RefreshTokenRequest refreshTokenRequest);
+}
