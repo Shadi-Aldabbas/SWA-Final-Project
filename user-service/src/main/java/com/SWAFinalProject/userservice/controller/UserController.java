3eb42e542decf1d35aa68023e17c38b950ccc3fe
Heran Aregawi <heranaregawi@gmail.com>
2022-06-12 00:46:11 -0500

    initial commit

---
+++ user-service/src/main/java/com/SWAFinalProject/userservice/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.SWAFinalProject.userservice.controller;
+
+import com.SWAFinalProject.userservice.model.User;
+import com.SWAFinalProject.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+       return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        return new ResponseEntity<>(userService.findAll(),HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable String id) throws Exception {
+        return new ResponseEntity<>(
+                userService.findById(id),
+                HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable String id, @RequestBody  User user) throws Exception {
+        return new ResponseEntity<>(userService.update(user, id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable String id) throws Exception {
+        userService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+}
