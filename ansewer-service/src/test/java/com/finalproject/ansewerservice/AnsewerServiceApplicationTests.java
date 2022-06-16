585e65c77fb3114dcdb64f8265f940e4369a9f38
hasimyil <hasim@hasimyilmaz.com>
2022-06-16 10:42:13 -0500

    Cassandra has been fixed and basic test implementation has been done

--- ansewer-service/src/test/java/com/finalproject/ansewerservice/AnsewerServiceApplicationTests.java
+++ ansewer-service/src/test/java/com/finalproject/ansewerservice/AnsewerServiceApplicationTests.java
@@ -1,13 +1,17 @@
 package com.finalproject.ansewerservice;
 
+import com.finalproject.ansewerservice.service.AnswerServiceTest;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AnsewerServiceApplicationTests {
 
+
 	@Test
 	void contextLoads() {
+
 	}
 
 }
